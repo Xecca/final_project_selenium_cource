045a129c2363ba0c34f7d36a6ffa45bb80e307df
Dreik <ensimgen@gmail.com>
2019-11-30 06:26:36 -0800

    added check exist login and registration form

---
+++ pages/login_page.py
@@ -0,0 +1,26 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert self.is_element_present(*LoginPageLocators.LOGIN_URL), "Login URL is not presented"
+        login_url = self.browser.find_element(*LoginPageLocators.LOGIN_URL)
+        login_url.click()
+        assert "ru/accounts/login/" in self.browser.current_url, "'ru/accounts/login/' not in current url"
+        print("\nclicked on link to forms!")
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login Form is not presented"
+        print("\nchecked exist login form!")
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Registration Form is not presented"
+        print("\nchecked exist registration form!")
---
+++ test_login_page.py
@@ -0,0 +1,9 @@
+from .pages.login_page import LoginPage
+
+def test_should_be_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+    page.should_be_login_form()
+    page.should_be_register_form()
